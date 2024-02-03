286f39be8a3db20cac6037055d0ff93cc075deb5
Deep Gaonkar <gaonkardeep890@gmail.com>
2024-02-03 14:51:41 +0530

    feat:Added creation of board and loading states

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,30 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import Image from "next/image";
+import { api } from "@/convex/_generated/api";
+import { useOrganization } from "@clerk/nextjs";
+import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { Button } from "@/components/ui/button";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board Created");
+        // TODO: Redirect to board/id
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/note.svg" alt="Empty" height={140} width={140} />
@@ -10,7 +33,9 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size="lg">Create Board</Button>
+        <Button disabled={pending} size="lg" onClick={onClick}>
+          Create Board
+        </Button>
       </div>
     </div>
   );
