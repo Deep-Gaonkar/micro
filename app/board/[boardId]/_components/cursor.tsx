128eea65aeed6634e89c144923dd2eba1c708c36
Deep Gaonkar <gaonkardeep890@gmail.com>
2024-04-23 11:06:24 +0000

    feat: added cursor presence, active users can see each others cursor with their names in real time

--- app/board/[boardId]/_components/cursor.tsx
+++ app/board/[boardId]/_components/cursor.tsx
@@ -1,26 +1,47 @@
-'use client'
+"use client";
 
-import { memo } from "react"
-import { MousePointer2 } from "lucide-react"
-
-import { connectionIdToColor } from "@/lib/utils"
+import { memo } from "react";
+import { MousePointer2 } from "lucide-react";
 import { useOther } from "@/liveblocks.config";
+
+import { connectionIdToColor } from "@/lib/utils";
 
 interface CursorProps {
   connectionId: number;
 }
 
 export const Cursor = memo(({ connectionId }: CursorProps) => {
-  const info = useOther(connectionId, (user) => user?.info)
-  const cursor = useOther(connectionId, (user) => user.presence.cursor)
+  const info = useOther(connectionId, (user) => user?.info);
+  const cursor = useOther(connectionId, (user) => user.presence.cursor);
 
-  const name = info?.name || "Teammate"
+  const name = info?.name || "Teammate";
 
-  if (!cursor) return null
+  if (!cursor) return null;
 
-  const { x, y } = cursor
+  const { x, y } = cursor;
 
   return (
-    <p></p>
-  )
-})
+    <foreignObject
+      style={{
+        transform: `translateX(${x}px) translateY(${y}px)`,
+      }}
+      height={50}
+      width={name.length * 10 + 24}
+      className="relative drop-shadow-md"
+    >
+      <MousePointer2
+        className="h5 w-5"
+        style={{
+          fill: connectionIdToColor(connectionId),
+          color: connectionIdToColor(connectionId),
+        }}
+      />
+      <div
+        className="absolute left-5 px-1.5 py-0.5 rounded-md text-xs text-white font-semibold"
+        style={{ backgroundColor: connectionIdToColor(connectionId) }}
+      >
+        {name}
+      </div>
+    </foreignObject>
+  );
+});
